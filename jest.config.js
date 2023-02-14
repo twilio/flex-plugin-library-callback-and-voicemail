03086c21a14ed81649a96b6d21212d03cc75aacf
pragya-arora <103407403+pragya-arora@users.noreply.github.com>
2023-02-14 19:03:39 +0530

    FLXUIAPPIN-583 add unit tests for serverless (#3)
    * FLXUIAPPIN-583 add unit tests for serverless
    
    * Restructure plugin for quick deploy (#5)
    
    * restructure plugin
    
    * restructure serverless code
    
    * restructure plugin
    
    * Restructured plugin for quick deploy
    
    * Add error notification
    
    * Add flag for error notification
    
    * Update package.json

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  rootDir: '.',
+  clearMocks: true,
+  setupFilesAfterEnv: [
+    "<rootDir>/setupTests.js"
+  ],
+  coveragePathIgnorePatterns: [
+    '.*\\.d\\.ts',
+    'index\\.ts',
+    'polyfilled\\.ts',
+    'createAction.ts',
+    'jest.config.js',
+    'webpack.*\\.js',
+    './coverage',
+    './public',
+    '/test-utils',
+    '/types',
+  ],
+  "roots": [
+    "<rootDir>"
+  ],
+  "modulePaths": [
+    "<rootDir>"
+  ],
+}
