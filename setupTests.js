2384e94334403dab8c99c594ab14f0b66d8698a8
pragya-arora <103407403+pragya-arora@users.noreply.github.com>
2023-03-07 15:04:57 +0530

    Add sonar integration (#11)
    * Updated jest config to collect coverage

--- setupTests.js
+++ setupTests.js
@@ -5,7 +5,6 @@ import { resetReduxState } from './test-utils/flex-redux';
 import { resetServiceConfiguration } from './test-utils/flex-service-configuration';
 
 process.env.FLEX_APP_SERVERLESS_FUNCTONS_DOMAIN = 'mockServerlessFunctionsDomain'
-jest.setTimeout(15000);
 
 // Global test lifecycle handlers
 beforeAll(() => {
