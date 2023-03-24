f4033fee034b89a7229a1f6c2d556682e2f88cd1
Pragya Arora <parora@twilio.com>
2023-03-24 12:34:33 +0530

    Added env variable for number of retry attempts

--- ui-src/src/flex-hooks/components/TaskInfoPanel.tsx
+++ ui-src/src/flex-hooks/components/TaskInfoPanel.tsx
@@ -6,8 +6,19 @@ import CallbackAndVoicemail from '../../components/CallbackAndVoicemail';
 export function replaceViewForCallbackAndVoicemail(flex: typeof Flex, manager: Flex.Manager) {
   
   try {
+    let maxRetryAttempts = Number(process.env.FLEX_APP_CALLBACK_RETRY_ATTEMPTS || '<FLEX_APP_CALLBACK_RETRY_ATTEMPTS>');
+    
+    if (isNaN(maxRetryAttempts)) {
+      maxRetryAttempts = 3; 
 
-    Flex.TaskInfoPanel.Content.replace(<CallbackAndVoicemail key="callback-component" maxAttempts={3} />, {
+      ErrorManager.createAndProcessError("Invalid number of retry attempts, setting it to default value of 3", {
+        type: FlexPluginErrorType.programabelComponents,
+        description: "Invalid number of retry attempts, setting it to default value of 3",
+        context: "Plugin.Component.TaskInfoPanel"
+      });
+    }
+
+    Flex.TaskInfoPanel.Content.replace(<CallbackAndVoicemail key="callback-component" maxAttempts={maxRetryAttempts} />, {
       sortOrder: -1,
       if: (props) => ['callback', 'voicemail'].includes(props.task.attributes.taskType),
     });
