fc91d1445dd5cdf747bfee58131aff1b68a2d952
sanand <sanand@twilio.com>
2023-08-22 13:39:14 +0530

    remove formatting

--- functions/callback/studio/create-callback.protected.js
+++ functions/callback/studio/create-callback.protected.js
@@ -1,15 +1,15 @@
-const { prepareStudioFunction } = require(Runtime.getFunctions()['helpers/prepare-function'].path);
+const { prepareStudioFunction } = require(Runtime.getFunctions()["helpers/prepare-function"].path);
 const { TaskRouterUtils } = require('@twilio/flex-plugins-library-utils');
 
 const requiredParameters = [
-  { key: 'numberToCall', purpose: 'the number of the customer to call' },
+  { key: "numberToCall", purpose: "the number of the customer to call" },
   {
-    key: 'numberToCallFrom',
-    purpose: 'the number to call the customer from',
+    key: "numberToCallFrom",
+    purpose: "the number to call the customer from",
   },
   {
-    key: 'flexFlowSid',
-    purpose: 'the SID of the Flex Flow that triggered this function',
+    key: "flexFlowSid",
+    purpose: "the SID of the Flex Flow that triggered this function",
   },
 ];
 
@@ -35,16 +35,17 @@ exports.handler = prepareStudioFunction(requiredParameters, async (context, even
     } = event;
 
     // use assigned values or use defaults
-    const workflowSid = overriddenWorkflowSid || process.env.TWILIO_FLEX_CALLBACK_WORKFLOW_SID;
+    const workflowSid =
+      overriddenWorkflowSid || process.env.TWILIO_FLEX_CALLBACK_WORKFLOW_SID;
     const timeout = overriddenTimeout || 86400;
     const priority = overriddenPriority || 0;
     const attempts = retryAttempt || 0;
-    const taskChannel = overriddenTaskChannel || 'voice';
-
+    const taskChannel = overriddenTaskChannel || "voice";
+    
     // setup required task attributes for task
     const attributes = {
-      taskType: RecordingSid ? 'voicemail' : 'callback',
-      name: (RecordingSid ? 'Voicemail' : 'Callback') + ` (${numberToCall})`,
+      taskType: RecordingSid ? "voicemail" : "callback",
+      name: (RecordingSid ? "Voicemail" : "Callback") + ` (${numberToCall})`,
       flow_execution_sid: flexFlowSid,
       message: message || null,
       callBackData: {
@@ -59,12 +60,12 @@ exports.handler = prepareStudioFunction(requiredParameters, async (context, even
         TranscriptionText,
         isDeleted: isDeleted || false,
       },
-      direction: 'inbound',
+      direction: "inbound",
       conversations: {
         conversation_id,
       },
     };
-
+    
     const TaskOperations = new TaskRouterUtils(context.getTwilioClient(), {
       ...process.env,
     });
